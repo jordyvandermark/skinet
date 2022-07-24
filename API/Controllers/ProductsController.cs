b45d9a94044815b229b73c3fbfa45f0bd7aab2ba
Jordy van der Mark <jordyvandermark@outlook.com>
2022-07-24 23:45:50 +0200

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,23 +1,29 @@
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class ProductsController : ControllerBase
+  public class ProductsController : BaseApiController
   {
+    // Fields initialized from constructor parameters
+    /*
+      A field is a variable of any type that is declared directly in a class or struct. Fields are members of their containing type.
+      Fields are initialized immediately before the constructor for the object instance is called. A read-only field can only be assigned a value during initialization or in a constructor. Fields typically store the data that must be accessible to more than one type method and must be stored for longer than the lifetime of any single method.
+    */
     private readonly IGenericRepository<Product> _productsRepo;
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
     private readonly IGenericRepository<ProductType> _productTypeRepo;
     private readonly IMapper _mapper;
 
+    // Constructor with parameters
+    /*
+      When a class or struct is instantiated, its constructor is called. Constructors have the same name as the class or struct, and they usually initialize the data members of the new object. Its method signature includes only an optional access modifier, the method name and its parameter list; it does not include a return type.
+    */
     public ProductsController(IGenericRepository<Product> productsRepo,
     IGenericRepository<ProductBrand> productBrandRepo, IGenericRepository<ProductType> productTypeRepo, IMapper mapper)
     {
@@ -27,22 +33,34 @@ namespace API.Controllers
       _productsRepo = productsRepo;
     }
 
+    // Async methods
+    /*
+      Async methods are intended to be non-blocking operations. An await expression in an async method doesn't block the current thread while the awaited task is running. Instead, the expression signs up the rest of the method as a continuation and returns control to the caller of the async method.
+      The async and await keywords don't cause additional threads to be created. Async methods don't require multithreading because an async method doesn't run on its own thread. The method runs on the current synchronization context and uses time on the thread only when the method is active.
+    */
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
     {
+      // Specification pattern alongside a generic Repository
       var spec = new ProductsWithTypesAndBrandsSpecification();
 
+      // Await keyword because of querying the database
       var products = await _productsRepo.ListAsync(spec);
 
+      // using Ok statement because of 'cannot implicitly convert list' error
       return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
       var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
       var product = await _productsRepo.GetEntityWithSpec(spec);
+
+      if(product == null) return NotFound(new ApiResponse(404));
 
       return _mapper.Map<Product, ProductToReturnDto>(product);
     }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,56 +1,58 @@
+using API.Extensions;
 using API.Helpers;
-using Core.Interfaces;
+using API.Middleware;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
   public class Startup
+  {
+    private readonly IConfiguration _config;
+    public Startup(IConfiguration config)
     {
-        private readonly IConfiguration _config;
-        public Startup(IConfiguration config)
-        {
-            _config = config;
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
-            services.AddAutoMapper(typeof(MappingProfiles));
-            services.AddControllers();
-            services.AddDbContext<StoreContext>(x =>
-                x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseStaticFiles();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      _config = config;
     }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddAutoMapper(typeof(MappingProfiles));
+      services.AddControllers();
+      services.AddDbContext<StoreContext>(x =>
+          x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+      services.AddApplicationServices();
+      services.AddSwaggerDocumentation();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      /*
+      Middleware is software that's assembled into an app pipeline to handle requests and responses. Each component:
+      -   Chooses whether to pass the request to the next component in the pipeline.
+      -   Can perform work before and after the next component in the pipeline. 
+      */
+
+      app.UseMiddleware<ExceptionMiddleware>();
+
+      app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseStaticFiles();
+
+      app.UseAuthorization();
+
+      app.UseSwaggerDocumentation();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
